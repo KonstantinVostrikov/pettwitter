3db1082529164a3056871f7e0781e023f72d4468
Konstantin Vostrikov <k.vostrikov@jusan.kz>
2025-07-10 01:35:20 +0500

    add custom login page

--- src/main/kotlin/vostrikov/pet/twitter/config/SecurityConfiguration.kt
+++ src/main/kotlin/vostrikov/pet/twitter/config/SecurityConfiguration.kt
@@ -2,12 +2,35 @@ package vostrikov.pet.twitter.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.JdbcUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
 import javax.sql.DataSource
+
 
 @Configuration
 class SecurityConfiguration {
+
+    @Bean
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { requests ->
+                requests
+                    .requestMatchers("/", "/feed", ).authenticated()
+                    .requestMatchers("/assets/**", "/login/**").permitAll()
+            }
+            .formLogin { formLoginConfig ->
+                formLoginConfig
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/feed")
+                    .failureUrl("/login?error=true")
+            }
+
+        return http.build()
+    }
 
     @Bean
     fun userDetailsService(dataSource: DataSource): UserDetailsService {
---
+++ src/main/kotlin/vostrikov/pet/twitter/controllers/LoginController.kt
@@ -0,0 +1,27 @@
+package vostrikov.pet.twitter.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@Controller
+class LoginController {
+
+    @GetMapping("/login")
+    fun displayLoginPage(
+        @RequestParam(value = "error", required = false) error: String?,
+        @RequestParam(value = "logout", required = false) logout: String?,
+        model: Model
+    ): String {
+        var errorMessge: String? = null
+        if (null != error) {
+            errorMessge = "Username or Password is incorrect!"
+        }
+        if (null != logout) {
+            errorMessge = "You have been successfully logged out!"
+        }
+        model.addAttribute("errorMessge", errorMessge)
+        return "login"
+    }
+}
